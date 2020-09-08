2ebfd4b358193c54660ce852fd94dae33a98fa51
Favarish <Favarish@yandex.ru>
2020-09-08 23:23:39 +0700

    Шляпа с кодировками 2 лаба

--- src/ru/favarish/Lab2.java
+++ src/ru/favarish/Lab2.java
@@ -4,10 +4,12 @@ import java.io.*;
 import java.math.BigInteger;
 import java.util.Random;
 
+import static java.lang.Byte.*;
+
 public class Lab2 {
     private static final int bitLength = 30;
 
-    public static void cipherShamir() throws FileNotFoundException {
+    public static void cipherShamir() throws IOException {
         Random random = new Random();
         Vector resultEuclid;
         FileReader readerOriginalFile = new FileReader("example.png");
@@ -20,7 +22,7 @@ public class Lab2 {
         } while (!p.isProbablePrime(3));
 
         do {
-            cA = BigInteger.probablePrime(bitLength, random);
+            cA = BigInteger.probablePrime(bitLength - 1, random);
         } while (!cA.isProbablePrime(3));
 
         resultEuclid = Lab1.generalizedEuclidsAlgorithm(p.subtract(BigInteger.ONE), cA);
@@ -31,8 +33,8 @@ public class Lab2 {
         dA = resultEuclid.y;
 
         do {
-            cB = BigInteger.probablePrime(bitLength, random);
-        } while (cB.isProbablePrime(3));
+            cB = BigInteger.probablePrime(bitLength - 1, random);
+        } while (!cB.isProbablePrime(3));
 
         resultEuclid = Lab1.generalizedEuclidsAlgorithm(p.subtract(BigInteger.ONE), cB);
 
@@ -41,12 +43,31 @@ public class Lab2 {
         }
         dB = resultEuclid.y;
 
-        try {
-            FileOutputStream writerEncoderFile = new FileOutputStream("encoder.png");
-//            name = fileInputStream.read();
-//            name = readerOriginalFile.read();
-        } catch (IOException e) {
-            e.printStackTrace();
+        byte[] readBytes = fileInputStream.readAllBytes();
+
+        try (/*FileWriter fileWriter = new FileWriter("encoder.png")*/BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("encoder.png"), "utf-8"))) {
+            for (byte i : readBytes) {
+                int intByte = i;
+                BigInteger x1 = Lab1.fastModuloExponentiation(new BigInteger(i + ""), cA, p);
+                BigInteger x2 = Lab1.fastModuloExponentiation(x1, cB, p);
+                BigInteger x3 = Lab1.fastModuloExponentiation(x2, dA, p);
+                String temp = x3.toString() + "\n";
+                bufferedWriter.write(temp.toCharArray());
+            }
         }
+
+        BufferedReader reader = new BufferedReader(new FileReader("encoder.png"));
+        ;
+        try (/*FileWriter fileWriter = new FileWriter("decoder.png")*/BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("decoder.png"), "UTF-8"))) {
+            String str;
+            while ((str = reader.readLine()) != null) {
+                BigInteger x4 = Lab1.fastModuloExponentiation(new BigInteger(str), dB, p);
+                int temp = Integer.valueOf(x4.toString());
+                bufferedWriter.write((char) temp);
+//                fileWriter.write((char) temp);
+            }
+        }
+
+
     }
 }
