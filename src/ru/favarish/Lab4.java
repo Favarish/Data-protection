934b95ebb5cda99173a06067ea9158839aef291f
Favarish <Favarish@yandex.ru>
2020-09-29 22:58:28 +0700

    init 4 lab with cards file

---
+++ src/ru/favarish/Lab4.java
@@ -0,0 +1,28 @@
+package ru.favarish;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Lab4 {
+
+//    public static void writeFileCards() throws IOException {
+//        FileWriter writer = new FileWriter("cards.txt");
+//
+//        String[] strs = {"П", "Ч", "Б", "К"};
+//
+//        for (int i = 0; i < strs.length; i++) {
+//            for (int j = 2; j < 11; j++) {
+//                writer.write(strs[i] + " " + j + "\n");
+//            }
+//            writer.write(strs[i] + " V\n");
+//            writer.write(strs[i] + " D\n");
+//            writer.write(strs[i] + " K\n");
+//            writer.write(strs[i] + " T\n");
+//        }
+//
+//        writer.close();
+//    }
+
+
+}
