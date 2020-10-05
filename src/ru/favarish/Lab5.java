c1300deff36dc8fab49554c9b1fd3abbd74dbc52
Favarish <Favarish@yandex.ru>
2020-10-05 22:57:37 +0700

    lab 5 half-completed

--- src/ru/favarish/Lab5.java
+++ src/ru/favarish/Lab5.java
@@ -1,7 +1,78 @@
 package ru.favarish;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.math.BigInteger;
+import java.util.Random;
+import java.util.Scanner;
 
 public class Lab5 {
-    
+    private static final int bitLength = 512;
+
+
+    public static void Vote() throws IOException {
+        Random random = new Random();
+        Scanner in = new Scanner(System.in);
+        BigInteger n, rnd, v, r, h, _h, _s, s;
+        Vector resultEuclid;
+        Integer voice = -1;
+        BigInteger[] bulletin = new BigInteger[2];
+
+        Server server = Server.startServer();
+
+        do {
+            try {
+                System.out.println("Выберите за что голосуете:\n");
+                System.out.println("0 - за джавку");
+                System.out.println("1 - за си");
+                voice = Integer.valueOf(in.nextLine());
+            } catch (NumberFormatException ex) {
+                System.out.println("Некорректный ввод. Попробуйте еще раз");
+            }
+        } while (voice < 0 || voice > 1);
+        //Пункт 1
+        rnd = BigInteger.probablePrime(bitLength, random);
+        v = new BigInteger(voice.toString() + Server.getAddress());
+
+        n = new BigInteger(rnd.toString() + v);
+
+        //Пункт 2
+        do {
+            r = BigInteger.probablePrime(30, random);
+            resultEuclid = Lab1.generalizedEuclidsAlgorithm(server.getN(), r);
+        } while (resultEuclid.gcd.compareTo(BigInteger.ONE) != 0);
+
+        //Пункт 3
+        h = new BigInteger(n.hashCode() + "");
+
+        //Пункт 4 и 5
+        _h = h.multiply(Lab1.fastModuloExponentiation(r, server.getD(), server.getN()));
+        _s = server.giveBulletin(_h);
+
+        //Пункт 6
+        s = _s.multiply(r.modInverse(server.getN()));
+
+        //Пункт 7
+        bulletin[0] = n;
+        bulletin[1] = s;
+
+        if (server.checkVoice(bulletin, "Alice")) {
+            System.out.println("Ваш голос успешно зачтен!");
+        } else {
+            System.out.println("Произошла ошибка при принятии голоса. Возможно, вы пытаетесь проголосовать второй раз");
+        }
+
+        readResultVote();
+    }
+
+    public static void readResultVote() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("Result Vote.txt"));
+        String tmp;
+
+        while ((tmp = reader.readLine()) != null) {
+            System.out.println(tmp);
+        }
+    }
 }
--- src/ru/favarish/Server.java
+++ src/ru/favarish/Server.java
@@ -1,11 +1,15 @@
 package ru.favarish;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Random;
 
 public class Server {
     private static Server single;
     private static final int bitLength = 1024;
+    private static final int address = 314159265;
+    private static FileWriter fileWriter;
     private BigInteger p;
     private BigInteger q;
     private BigInteger n;
@@ -13,9 +17,10 @@ public class Server {
     private BigInteger c;
     private BigInteger d;
 
-    private Server() {
+    private Server() throws IOException {
         Random random = new Random();
         Vector resultEuclid;
+        fileWriter = new FileWriter("Result Vote.txt");
 
         do {
             q = BigInteger.probablePrime(bitLength, random);
@@ -43,11 +48,37 @@ public class Server {
         c = resultEuclid.y;
     }
 
-    public static Server startServer() {
+    public BigInteger giveBulletin(BigInteger _h) {
+        return Lab1.fastModuloExponentiation(_h, c, n);
+    }
+
+    public boolean checkVoice(BigInteger[] bulletin, String name) throws IOException {
+        char voteChar = bulletin[0].toString(2).charAt(513);
+        String vote = new String();
+
+        if (voteChar == '0') {
+            vote = "За си";
+        } else if (voteChar == '1') {
+            vote = "За Джавку";
+        }
+
+        if (new BigInteger(bulletin[0].hashCode() + "").compareTo(Lab1.fastModuloExponentiation(bulletin[1], d, n)) == 0) {
+            fileWriter.write(name + " " + vote);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static Server startServer() throws IOException {
         if (single == null) {
             single = new Server();
         }
         return single;
+    }
+
+    public static int getAddress() {
+        return address;
     }
 
     public BigInteger getN() {
